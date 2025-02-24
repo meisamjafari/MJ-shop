1cb9161cbc3f133f780fb6f8a87a42f300022f41
meisamjafari <143758356+meisamjafari@users.noreply.github.com>
2025-02-24 17:38:31 +0330

    اضافه کردن ساختار اولیه پروژه

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer' 
+
+
+function App() {
+  
+
+  return (
+    <>
+  <Header/>
+  <Home/> 
+  <Footer/>     
+    </>
+  )
+}
+
+export default App
